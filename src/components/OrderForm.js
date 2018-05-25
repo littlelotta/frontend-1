a2929cfdffa217771b5c7277e8b8c6a7402c2056
Lincoln de Sousa <lincoln@comum.org>
2018-05-25 15:59:53 -0400

    feat(OrderForm): Show organization payment methods

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -198,7 +198,9 @@ class OrderForm extends React.Component {
   paymentMethodsOptionsForCollective = (paymentMethods, collective) => {
     return paymentMethods.map(pm => {
       const value = pm.uuid
-      const label = `💳  \xA0\xA0${collective.name} - ${get(pm, 'data.brand')} ${pm.name} - exp ${get(pm, 'data.expMonth')}/${get(pm, 'data.expYear')}`;
+      const brand = get(pm, 'data.brand') || get(pm, 'type');
+      const expiration = get(pm, 'expiryDate') || `${get(pm, 'data.expMonth')}/${get(pm, 'data.expYear')}`;
+      const label = `💳  \xA0\xA0${collective.name} - ${brand} ${pm.name} - exp ${expiration}`;
       const option = {};
       option[value] = label;
       return option;
@@ -212,8 +214,9 @@ class OrderForm extends React.Component {
 
     const collective = this.collectivesById[CollectiveId];
 
-    const filterPMs = input => (input || [])
-      .filter((pm) => ['stripe', 'paypal'].includes(pm.service));
+    const filterPMs = (pms) => (pms || []).filter(pm =>
+      ((pm.service === 'stripe' || pm.service === 'paypal') ||
+       (pm.service === 'opencollective' && pm.type === 'prepaid')));
 
     if (collective) {
       const paymentMethods = filterPMs(collective.paymentMethods);
@@ -507,7 +510,7 @@ class OrderForm extends React.Component {
           this.setState({ result: { error: intl.formatMessage(this.messages['prepaidcard.amounterror'])}});
           return false;
         }
-        newState.paymentMethod = { token: prepaidcard.token,  service: 'prepaid', uuid: prepaidcard.uuid };
+        newState.paymentMethod = { token: prepaidcard.token,  service: 'opencollective', type: 'prepaid', uuid: prepaidcard.uuid };
         this.setState(newState);
         return true;
 
